29b463f464126a2a9cf7a1409175cddbc0f43825
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-26 19:50:15 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,29 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    sailboat = Classification.find_by(name:"Sailboat")
+    Captain.select do |captain|
+      captain.boats.detect {|boat| boat.classifications.include?(sailboat)}
+    end
+  end
+
+  def self.talented_seafarers
+    sailboat = Classification.find_by(name:"Sailboat")
+    motorboat = Classification.find_by(name:"Motorboat")
+    Captain.select do |captain|
+      captain.boats.detect do |boat|
+        boat.classifications.include?(sailboat) &&
+        boat.classifications.include?(motorboat)
+      end
+    end
+  end
+
+  def self.non_sailors
+    Captain.joins(boats: :classifications).where.not("classifications.name = 'Sailboat'").distinct
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    # returns all classifications (FAILED - 8)
+  end
+
+  def self.longest
+    # returns the classifications for the longest boat (FAILED - 9)
+  end
+
 end
